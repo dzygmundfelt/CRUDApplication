4b875caab9400cc50c5a7741282207c9c550e430
Dan Zygmund-Felt <d.zygmund.felt@gmail.com>
2017-06-15 15:32:02 -0400

    created Person class

---
+++ src/main/java/io/zipcoder/crudapp/Person.java
@@ -0,0 +1,19 @@
+package io.zipcoder.crudapp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by danzygmund-felt on 6/15/17.
+ */
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    
+
+}
