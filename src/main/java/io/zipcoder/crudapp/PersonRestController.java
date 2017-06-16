fcec95b244095fd78a49fc7f1b2299ff57ac1a38
Dan Zygmund-Felt <d.zygmund.felt@gmail.com>
2017-06-15 21:14:15 -0400

    added Put methods

--- src/main/java/io/zipcoder/crudapp/PersonRestController.java
+++ src/main/java/io/zipcoder/crudapp/PersonRestController.java
@@ -21,13 +21,35 @@ public class PersonRestController {
     }
 
     @RequestMapping(value = "/person", method = RequestMethod.POST)
-    public void addPerson(@RequestParam String name, @RequestParam Integer age) {
-        people.save(new Person(name, age));
+    public Person addPerson(@RequestParam String name, @RequestParam Integer age) {
+        return people.save(new Person(name, age));
     }
 
     @RequestMapping(value = "/person/{id}", method = RequestMethod.DELETE)
     public void deletePerson(@PathVariable Integer id) {
         people.delete(id);
+    }
+
+    @RequestMapping(value = "/person/{id}", method = RequestMethod.PUT)
+    public Person modifyPerson(@PathVariable Integer id, @RequestParam String name, @RequestParam Integer age) {
+        Person person = people.findOne(id);
+        person.setName(name);
+        person.setAge(age);
+        return person;
+    }
+
+    @RequestMapping(value = "/person/{id}", method = RequestMethod.PUT)
+    public Person modifyPerson(@PathVariable Integer id, @RequestParam String name) {
+        Person person = people.findOne(id);
+        person.setName(name);
+        return person;
+    }
+
+    @RequestMapping(value = "/person/{id}", method = RequestMethod.PUT)
+    public Person modifyPerson(@PathVariable Integer id, @RequestParam Integer age) {
+        Person person = people.findOne(id);
+        person.setAge(age);
+        return person;
     }
 
 }
